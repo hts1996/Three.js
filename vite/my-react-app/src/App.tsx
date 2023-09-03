d2d46fc13c6388744642082c46ad7c27559cc60d
HT <tgx963@naver.com>
2023-09-03 22:35:58 +0900

    vite

---
+++ vite/my-react-app/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card.tsx'
+function App() {
+
+  return (
+    <>
+    <div id="card1">
+    <Card />
+    <Card />
+    <Card />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite/my-react-app/src/Card.tsx
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Box } from '@react-three/drei';
+import './Card.css'
+
+function Cube() {
+  const cubeRef = useRef();
+
+  useFrame(({ clock }) => {
+    // 시간에 따라 자동으로 회전
+    cubeRef.current.rotation.y += 0.005; // 회전 속도 조절
+  });
+
+  return (
+    <Box args={[2, 3, 0.02]} position={[0, 0, 0]} rotation={[0, 0, 0]} ref={cubeRef}>
+      <meshPhongMaterial attach="material" color={0x44a88} />
+    </Box>
+  );
+}
+
+function App() {
+  return (
+    <div>
+    <Canvas>
+      <ambientLight intensity={1} />
+      <directionalLight position={[1, 2, 4]} intensity={1} />
+      <Cube />
+      <OrbitControls />
+    </Canvas>
+    </div>
+  );
+}
+
+export default App;
